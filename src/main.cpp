c637c1184d42d7ca3359aad18e374c7c90499def
mgr401018 <martin.v.grigorov.2021@elsys-bg.org>
2025-07-04 13:07:26 +0300

    triangle

--- src/main.cpp
+++ src/main.cpp
@@ -48,25 +48,47 @@ int main(void)
     //     0.5, 0.0
     // };
 
-    uint32_t gltfPositionIndex = model.meshes[0].primitives[0].attributes["POSITION"];
-    uint32_t gltfBufferIndex = model.bufferViews[gltfPositionIndex].buffer;
-    unsigned char* gltfBufferData = model.buffers[gltfBufferIndex].data.data();
+    GLfloat vertex[] = {
+        -0.5f, -0.5f, 0.0f, // positions[0]​
+        0.0f, 0.0f, 1.0f, // normals[0]​
+        0.25f, 0.25f, // texture coordinates[0]​
+        0.5f, -0.5f, 0.0f, // positions[1]​
+        0.0f, 0.0f, 1.0f, // normals[1]​
+        0.75f, 0.25f, // texture coordinates[1]​
+        0.0f, 0.5f, 0.0f, // positions[2]​
+        0.0f, 0.0f, 1.0f, // normals[2]​
+        0.5f, 0.75f // texture coordinates[2]​
+    };
 
-    float* vertex = (float*)gltfBufferData;
-    for(int i = 0; i < 9; i++){
-        std::cout << vertex[i] << std::endl;
-    }
+    // uint32_t gltfPositionIndex = model.meshes[0].primitives[0].attributes["POSITION"];
+    // uint32_t gltfBufferIndex = model.bufferViews[gltfPositionIndex].buffer;
+    // unsigned char* gltfBufferData = model.buffers[gltfBufferIndex].data.data();
+
+    // float* vertex = (float*)gltfBufferData;
+    // for(int i = 0; i < 9; i++){
+    //     std::cout << vertex[i] << std::endl;
+    // }
 
     unsigned int buffer = 0;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), vertex, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), 0);
+    
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(3 * sizeof(GL_FLOAT)));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(6 * sizeof(GL_FLOAT)));
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClearColor(0.0F, 1.0F, 0.0F, 1.0F);
+        //glClearColor(0.0F, 1.0F, 0.0F, 1.0F);
+        glClearColor(0.251F, 0.510F, 0.427F, 1.0F);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // glBegin(GL_TRIANGLES);
